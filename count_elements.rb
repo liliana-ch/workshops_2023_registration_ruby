2597f60184f47784a982729d74edc5f2b53d2e09
liliana-ch <liliana.cholody@gmail.com>
2023-04-24 18:54:17 +0200

    Zadania wykonane

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,15 @@
 class CountElements
+  def initialize(array)
+    @array = array
+  end
+
+  def perform
+    counts = Hash.new(0)
+    @array.each { |element| counts[element] += 1 }
+    puts counts
+  end
 
 end
+
+
+CountElements.new([0,4,2,1,0,2,5]).perform
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,21 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def perform
+    if @password.length >= 6 && @password.length <= 24 && @password.match(/[A-Z]/) && @password.match(/[a-z]/) && @password.match(/[0-9]/) &&  @password.match(/[!@#$%&*+=:;?<>]/) && @password.scan(/(.)\1{2,}/).length == 0
+      result = true
+    else
+      result = false
+    end
+    puts result
+  end
 end
+
+ValidatePassword.new("Password1!").perform
+
+ValidatePassword.new("password1!").perform
+ValidatePassword.new("Passsword1!").perform
+ValidatePassword.new("Password1").perform
+ValidatePassword.new("Pas1!").perform
